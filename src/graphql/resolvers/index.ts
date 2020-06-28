803c405ed6ea02540ed426fb212d0973f1a9e513
kevin <kevin.nin92@gmail.com>
2020-06-28 22:06:29 +0530

    initial

---
+++ src/graphql/resolvers/index.ts
@@ -0,0 +1,5 @@
+import merge from "lodash.merge";
+import { listingResolvers } from "./Listing";
+import { userResolvers } from "./User";
+
+export const resolvers = merge(userResolvers, listingResolvers);
